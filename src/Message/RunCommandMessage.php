ea8ae94759c64fadbe1ae275b28704352a277126
tourze-ci <you@example.com>
2025-03-25 15:42:54 +0000

    refactor(Message): 添加 RunCommandMessage 的注释和 PHPDoc

--- src/Message/RunCommandMessage.php
+++ src/Message/RunCommandMessage.php
@@ -2,27 +2,58 @@
 
 namespace Tourze\Symfony\AsyncMessage\Message;
 
+/**
+ * 异步运行命令的消息类
+ *
+ * 该类用于封装需要异步执行的命令及其选项
+ */
 class RunCommandMessage implements AsyncMessageInterface
 {
+    /**
+     * 要执行的命令
+     */
     private string $command;
 
+    /**
+     * 获取命令字符串
+     *
+     * @return string 命令字符串
+     */
     public function getCommand(): string
     {
         return $this->command;
     }
 
+    /**
+     * 设置要执行的命令
+     *
+     * @param string $command 命令字符串
+     */
     public function setCommand(string $command): void
     {
         $this->command = $command;
     }
 
+    /**
+     * 命令执行的选项
+     */
     private array $options = [];
 
+    /**
+     * 获取命令选项
+     *
+     * @return array 命令选项数组
+     */
     public function getOptions(): array
     {
         return $this->options;
     }
 
+    /**
+     * 设置命令选项
+     *
+     * @param array $options 命令选项数组
+     */
     public function setOptions(array $options): void
     {
         $this->options = $options;
